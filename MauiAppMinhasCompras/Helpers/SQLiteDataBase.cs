192a20db854f7ba539d82029e7a7a0e1d43c3957
Usuario <Usuario@CPD03-M08>
2024-11-04 15:11:46 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Helpers/SQLiteDataBase.cs
@@ -0,0 +1,27 @@
+﻿using MauiAppMinhasCompras.Models;
+using SQLite;
+
+namespace MauiAppMinhasCompras.Helpers
+{
+    public class SQLiteDataBaseHelper
+    {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDataBaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+
+        public Task<List<Produto>> Update(Produto p)
+        {
+            string sql = "UPDATE Produto SET Descricao=?, " +
+                "Quantidade=?, Preco=? WHERE Id=?";
+            return _conn.QueryAsync<Produto>(
+                sql, p.Descricao, p.Quantidade, p.Preco, p.Id
+                );
+        }
+
+    }
+}
+
